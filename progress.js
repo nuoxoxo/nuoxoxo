70d3af73d9013c1b8c77a5ddc3685f70ae307aaf
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2024-05-07 17:02:58 +0200

    Update progress.js

--- progress.js
+++ progress.js
@@ -72,9 +72,7 @@ Here's a showcase of recent projects that I've developed to tackle various tech 
 
 ![](https://github.com/nuoxoxo/nuoxoxo/blob/main/.github/assets/github-contribution-grid-snake.svg)
 
-<img style='display: block; margin: auto;'
-  src='https://github-readme-stats.vercel.app/api/top-langs/?username=nuoxoxo&layout=compact&hide=roff,html&langs_count=32' align='center'
->
+<img style='display: block; margin: auto;' src='https://github-readme-stats.vercel.app/api/top-langs/?username=nuoxoxo&layout=compact&hide=roff,html&langs_count=32' align='center'>
 \
 `
 
