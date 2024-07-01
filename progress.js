0df2580c5e86b87965c584c7fd5c8a2730d828ec
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2024-07-01 09:01:21 +0200

    Update progress.js

--- progress.js
+++ progress.js
@@ -8,7 +8,7 @@ const makeBar = () => {
 
     const N = 32
     const idx = parseInt(proportion * N)
-    const bar = '@'.repeat(idx) + '_'.repeat(N - idx)
+    const bar = '@'.repeat(idx) + 'ø'.repeat(N - idx)
     return `:: ${bar} ::`
 }
 
