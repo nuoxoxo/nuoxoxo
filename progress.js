00e36b34d06fa3e41318eba6ad480b9812644747
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2024-10-05 22:11:00 +0200

    Update progress.js

--- progress.js
+++ progress.js
@@ -34,7 +34,7 @@ const README = `\
 
 ## 🎯 &nbsp; Recent assignments
 Here's a showcase of recent projects that I've developed to tackle various tech interview tasks
-- ***[AssemblyLine](nuoxoxo.github.io/interview_react_assembly_line)*** - Assembly line demo 
+- ***[AssemblyLine](https://nuoxoxo.github.io/interview_react_assembly_line)*** - Assembly line demo 
 - ***[Live search](https://nuoxoxo.github.io/interview_react_search_filter)*** - A dynamic search filter 
 - ***[Confirmation](https://nuoxoxo.github.io/interview_react_confirmation_component)*** - Button set featuring notification bars _(notistack/snackbar)_
 
