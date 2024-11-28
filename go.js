0c1f1dd1009aee009a08ffe47335735e6d33ad7c
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2024-11-28 21:19:15 +0100

    Update go.js

--- go.js
+++ go.js
@@ -84,7 +84,7 @@ Here's a showcase of recent projects that I've developed to tackle various tech 
 
 ![](https://github.com/nuoxoxo/nuoxoxo/blob/main/.github/assets/github-contribution-grid-snake.svg)
 
-<img style='display: block; margin-left: auto; margin-right: auto; width: 50%;' src='https://github-readme-stats.vercel.app/api/top-langs/?username=nuoxoxo&layout=compact&hide=roff,html&langs_count=12'> \
+<img style='display: block; margin-left: auto; margin-right: auto; width: 50%;' src='https://github-readme-stats.vercel.app/api/top-langs/?username=nuoxoxo&layout=compact&hide=roff,html'> \
 <!-- <img src='https://deno.com/images/artwork/HypnoDeno.gif?__frsh_c=dad2' width='50%'/> --> \
 
 \
