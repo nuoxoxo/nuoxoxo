4fd944583381c23ce807ffd753a3ed9fc54eca2f
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2025-05-08 10:41:40 +0200

    Update go.js

--- go.js
+++ go.js
@@ -28,6 +28,7 @@ const README = `\
 
 ## 🚴‍♂️ &nbsp; current projects:
 
+
 - ***Snow Crash***
   - [solutions](https://github.com/nuoxoxo/snowcrash) 
   - [speedrun](https://github.com/nuoxoxo/snowcrash_speedrun) 
@@ -42,7 +43,32 @@ const README = `\
   - [site](https://www.pythonchallenge.com/) 
 
 
-## 🎯 &nbsp; Recent assignments
+## 🧩 &nbsp; Other projects
+
+
+- ***[Advent of Code](https://github.com/nuoxoxo/advent-of-code)***
+  - My solutions for Advent of Code challenges spanning 2015 - 2023 🌟🎄
+  - \`C++\` \`Python3\` \`TypeScript\` \`Go\` \`C\`
+- ***[Cryptopals](https://cryptopals.com/)***
+  - A Set of Crypto Challenges
+  - My [solutions](https://github.com/nuoxoxo/cryptopals) 
+- ***[Hanukkah of Data](https://github.com/nuoxoxo/hanukkah-of-data)***
+  - Writeups / solutions for _[Noah's Rug - 5784](https://hanukkah.bluebird.sh/)_ involving data probing and analytics, akin to a detective's investigation.
+  - \`Jupyter Notebook\` 
+- ***[Coding Quest](https://github.com/nuoxoxo/coding-quest)***
+  - Solutions for _[codingquest.io](https://codingquest.io/)_ over the past 3 years, providing insights into my problem-solving approach.
+  - \`Python\` \`Go\` and the intriguingly powerful \`Bash\` & \`AWK\`
+- ***[April Fools 2O23](https://github.com/nuoxoxo/april_event_2O23)***
+  - Solutions for April Fools Event @ 42 worldwide, Collaborating with some of my favorite colleagues made it an unforgettable time.
+  - \`Perl\` \`PHP\` \`C++\` \`TypeScript\` 
+- ***[LeetCode](https://github.com/nuoxoxo/leetcode)***
+  - LeetCode isn't a goal; it's a stepping stone toward achieving your goals. I keep these for the pure sake of keeping, documenting my journey through algorithmic challenges and continuous improvement. Currently the repo amounts to 2500+ code snippets covering over 800 problems.
+  - \`C++\` \`Go\` \`Python3\` \`TS/JS\` \`C\`
+- More ***[projects](https://github.com/nuoxoxo?tab=repositories)*** using \`Astro\` \`Deno\` \`Vanilla JavaScript\`  etc
+
+
+## 🎯 &nbsp; recent interview assignments
+
 
 Here's a showcase of recent projects that I've developed to tackle various tech interview tasks
 - ***[AssemblyLine](https://nuoxoxo.github.io/interview_react_assembly_line)*** - Assembly line demo 
@@ -69,28 +95,6 @@ Here's a showcase of recent projects that I've developed to tackle various tech 
 - ***[Ranch](https://nuoxoxo.github.io/ranch)***
   - Simple CSS playground for experiments  
 
-
-## 🧩 &nbsp; Long-term projects and codebases
-
-- ***[LeetCode](https://github.com/nuoxoxo/leetcode)***
-  - LeetCode isn't a goal; it's a stepping stone toward achieving your goals. I keep these for the pure sake of keeping, documenting my journey through algorithmic challenges and continuous improvement. Currently the repo amounts to 2500+ code snippets covering over 800 problems.
-  - \`C++\` \`Go\` \`Python3\` \`TS/JS\` \`C\`
-- ***[Advent of Code](https://github.com/nuoxoxo/advent-of-code)***
-  - My solutions for Advent of Code challenges spanning 2015 - 2023 🌟🎄
-  - \`C++\` \`Python3\` \`TypeScript\` \`Go\` \`C\`
-- ***[Cryptopals](https://cryptopals.com/)***
-  - A Set of Crypto Challenges
-  - My [solutions](https://github.com/nuoxoxo/cryptopals) 
-- ***[Hanukkah of Data](https://github.com/nuoxoxo/hanukkah-of-data)***
-  - Writeups / solutions for _[Noah's Rug - 5784](https://hanukkah.bluebird.sh/)_ involving data probing and analytics, akin to a detective's investigation.
-  - \`Jupyter Notebook\` 
-- ***[Coding Quest](https://github.com/nuoxoxo/coding-quest)***
-  - Solutions for _[codingquest.io](https://codingquest.io/)_ over the past 3 years, providing insights into my problem-solving approach.
-  - \`Python\` \`Go\` and the intriguingly powerful \`Bash\` & \`AWK\`
-- ***[April Fools 2O23](https://github.com/nuoxoxo/april_event_2O23)***
-  - Solutions for April Fools Event @ 42 worldwide, Collaborating with some of my favorite colleagues made it an unforgettable time.
-  - \`Perl\` \`PHP\` \`C++\` \`TypeScript\` 
-- More ***[projects](https://github.com/nuoxoxo?tab=repositories)*** using \`Astro\` \`Deno\` \`Vanilla JavaScript\`  etc
 
 ---
 
